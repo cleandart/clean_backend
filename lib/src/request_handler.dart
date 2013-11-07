887876be0a30cb54bd6ed4301e9a890f8f7d6301
matystl <mfedak@gmail.com>
2013-11-07 17:21:57 +0100

    RequestHandler no longer throw but return future with error

--- lib/src/request_handler.dart
+++ lib/src/request_handler.dart
@@ -7,34 +7,29 @@ part of clean_backend;
 typedef Future RequestExecutor(request);
 
 class RequestHandler {
- 
-  final Map<String, RequestExecutor> _registeredExecutors = new Map();
-  
-  bool get isEmpty => _registeredExecutors.isEmpty;
-  
- Future handleRequest(String name, request){
 
+  final Map<String, RequestExecutor> _registeredExecutors = new Map();
+
+  bool get isEmpty => _registeredExecutors.isEmpty;
+
+ Future handleRequest(String name, request){
    if(_registeredExecutors.containsKey(name)){
      return _registeredExecutors[name](request);
    }
-   
    if(_registeredExecutors.containsKey('')){
      return _registeredExecutors[''](request);
    }
-   
-   throw new Exception("Uknown Request");
-   //return new Future();
+   return new Future.error("Unknow request");
  }
- 
+
  bool registerExecutor(String name, RequestExecutor requestExecutor){
-   
    if(_registeredExecutors.containsKey(name)){
      return false;
    }
    _registeredExecutors[name] = requestExecutor;
    return true;
  }
- 
+
  bool unregisterExecutor(String name){
    if(!_registeredExecutors.containsKey(name)){
      return false;
