3ab44af88fcfc1caa551ee4bf89532e689fec0d9
matystl <mfedak@gmail.com>
2013-11-07 17:43:24 +0100

    removed unuserd files
    This files belong to sever side of clean_sync

--- lib/clean_backend.dart
+++ lib/clean_backend.dart
@@ -7,13 +7,9 @@ library clean_backend;
 import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
-import 'package:mongo_dart/mongo_dart.dart';
 import 'package:route/server.dart';
 import 'package:http_server/http_server.dart';
 import 'package:static_file_handler/static_file_handler.dart';
 
-part 'src/data_provider.dart';
-part 'src/mongo_provider.dart';
-part 'src/publisher.dart';
 part 'src/backend.dart';
 part 'src/request_handler.dart';
