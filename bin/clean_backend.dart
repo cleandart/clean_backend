5a072e1eda97643aaed5da910a46c95a74a7b955
matystl <mfedak@gmail.com>
2013-11-11 20:16:21 +0100

    removing request handler

--- bin/clean_backend.dart
+++ bin/clean_backend.dart
@@ -8,11 +8,13 @@ import '../lib/clean_backend.dart';
 import 'package:yaml/yaml.dart';
 import 'package:static_file_handler/static_file_handler.dart';
 
+import 'package:clean_ajax/clean_server.dart';
+
 void main() {
   Backend backend;
  // YamlMap config = loadConfig();
 
-  StaticFileHandler fileHandler = new StaticFileHandler.serveFolder('C:/Users/MatúšMaty/Documents/GitHub/');
+  StaticFileHandler fileHandler = new StaticFileHandler.serveFolder('/home/maty/vacuumlabs/git/');
   RequestHandler requestHandler = new RequestHandler();
 
   backend = new Backend( fileHandler, requestHandler)..listen();
--- lib/clean_backend.dart
+++ lib/clean_backend.dart
@@ -6,7 +6,6 @@ library clean_backend;
 
 import 'dart:io';
 import 'dart:async';
-import 'dart:convert';
 import 'package:route/server.dart';
 import 'package:http_server/http_server.dart';
 import 'package:static_file_handler/static_file_handler.dart';
